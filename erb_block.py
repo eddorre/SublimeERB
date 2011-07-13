352230f3da5d86d798b1149f6eee912ef3049249
Carlos Rodriguez <carlos@eddorre.com>
2011-07-13 00:03:13 -0700

    Initial Commit

---
+++ erb_block.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin
+
+class ErbCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+
+    print len(self.view.sel())
+    if len(self.view.sel()) != 1:
+      return
+
+    region = self.view.sel()[0]
+    if region.empty():
+      # Nothing is selected
+      self.view.insert(edit, region.begin(), "<%= %>")
+    else:
+      currentWord = self.view.substr(region)
+      self.view.replace(edit, region, " <%%= %s %%> " % currentWord)
