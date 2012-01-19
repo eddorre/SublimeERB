76069a540f834ff77ced24ee0d3d7f09a768dff1
Carlos Rodriguez <carlos@eddorre.com>
2012-01-19 01:09:13 -0800

    No longer inserting whitespace before and after an erb block. fixes #3

--- erb_block.py
+++ erb_block.py
@@ -36,30 +36,15 @@ class ErbCommand(sublime_plugin.TextCommand):
   def insert_erb_block(self, edit, erb_block):
     region = self.view.sel()[0]
 
-    left_most_character_region = sublime.Region(region.begin() - 1, region.begin())
-    left_most_character = self.view.substr(left_most_character_region)
-    right_most_character_region = sublime.Region(region.begin(), region.end() + 1)
-    right_most_character = self.view.substr(right_most_character_region)
-
-    if re.match('\s', left_most_character):
-      prefix = ""
-    else:
-      prefix = " "
-
-    if re.match('\s', right_most_character):
-      suffix = ""
-    else:
-      suffix = " "
-
-    self.view.insert(edit, region.begin(), prefix + erb_block + suffix)
+    self.view.insert(edit, region.begin(), erb_block)
 
     self.view.sel().clear()
     if len(erb_block) > 6:
-      offset = 4 - len(suffix)
+      offset = 4
     else:
-      offset = 3 - len(suffix)
+      offset = 3
 
-    self.view.sel().add(sublime.Region(region.begin() + len(prefix) + offset + len(suffix)))
+    self.view.sel().add(sublime.Region(region.begin() + offset))
 
   def replace_erb_block(self, edit):
     region = self.view.find(ERB_REGEX, self.view.sel()[0].begin() - 4)
